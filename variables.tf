a9ca1d3fa1b82969addefd95be9b65cf6a6e8a14
Aadil Abbasi <aadil.abbasi@cchellenic.com>
2025-07-28 12:36:52 -0400

    updated main.tf and variables.tf files with new defaults

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "subscription_id" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-cloudinit-demo"
+  default     = "rg-cloudinit-test"
 }
 
 variable "location" {
